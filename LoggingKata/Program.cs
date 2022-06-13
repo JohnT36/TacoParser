952423a2df920c741c56385222cb363f8f08c191
John Tuck <j.tuck1336@gmail.com>
2022-06-13 14:33:49 -0400

    Finished Project

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -35,16 +35,19 @@ namespace LoggingKata
 
             ITrackable tacoOne = null;
             ITrackable tacoTwo = null;
+            var count = 0;
+            var count2 = 0;
             double distance = 0;
 
             foreach (var locA in locations)
             {
                 var coordA = new GeoCoordinate(locA.Location.Latitude, locA.Location.Longitude);
-
+                count++;
                 foreach (var locB in locations)
                 {
                     var coordB = new GeoCoordinate(locB.Location.Latitude, locB.Location.Longitude);
                     var dist = coordA.GetDistanceTo(coordB);
+                    count2++;
                     if (dist > distance)
                     {
                         distance = dist;
@@ -60,8 +63,9 @@ namespace LoggingKata
             Console.WriteLine($"Second Location:{tacoTwo.Name}," +
                 $" Long: {tacoTwo.Location.Longitude}, Lat: {tacoTwo.Location.Latitude}");
 
-            Console.WriteLine(distance);                                   
-            
+            Console.WriteLine($"{Math.Round(distance * 0.00062)} miles");
+            Console.WriteLine($"{count} loops for the first loop");
+            Console.WriteLine($"{count2} loops for the second loop");
         }
     }
 }
